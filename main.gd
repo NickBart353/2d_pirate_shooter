243ccb9a6825cf09e17145689aa377d20ec61143
Nicholas Bartlett <nicholas_bartlett_30.09@outlook.de>
2025-10-02 12:18:35 +0200

    canon + rotation, canon fire charge, ball flies towards charged target

--- main.gd
+++ main.gd
@@ -1,10 +1,29 @@
 extends Node
 
 @export var cannonball_scene : PackedScene
+@export var canon_scene : PackedScene
 
-func _on_player_shoot_cannonball(position: Variant, target: Variant, player: Variant) -> void:
+var canon
+
+func _ready():
+	canon = canon_scene.instantiate()
+	add_child(canon)
+	$Target.visible = false
+
+func _on_player_shoot_cannonball(position: Variant, target: Variant, player: Variant, shooting_dir: Variant, charge: Variant) -> void:
 	var cannonball_instance = cannonball_scene.instantiate()
 	cannonball_instance.start_position = position
-	cannonball_instance.target = target
+	cannonball_instance.target = (shooting_dir * charge) + position
 	cannonball_instance.player = self
 	add_child(cannonball_instance)
+	$Target.visible = false
+
+func _on_player_rotate_player_canon(mousepos: Variant, player_pos: Variant) -> void:
+	var dir_vec = player_pos.direction_to(mousepos)
+	canon.global_position = player_pos + (dir_vec * 25) 
+	canon.look_at(mousepos)
+	#look at mit player_pos machen
+
+func _on_player_charge_player_canon(charge: Variant, charging_dir: Variant, player_pos: Variant) -> void:
+	$Target.visible = true
+	$Target.position = player_pos + (charging_dir * charge)
--- player.gd
+++ player.gd
@@ -5,14 +5,27 @@ extends CharacterBody2D
 @export var friction = 100.0
 @export var max_line_length = 100
 @export var cannonball_scene : PackedScene
-signal shoot_cannonball(position, target, player)
+@export var canon_scene : PackedScene
+signal shoot_cannonball(position, target, player, shooting_dir, charge)
+signal rotate_player_canon(mousepos, player_pos)
+signal charge_player_canon(charge, charging_dir, player_pos)
 
-var ball : PackedScene
+var charge_max = 200
+var charge_start = 50
+var charge_current = -1
+var charge_speed = 100
+var max_ammo = 3
+var current_ammo = max_ammo
 var direction = Vector2.ZERO
 var current_position = Vector2.ZERO
+var canon
+var charging : bool = false
+var shooting_dir
 
 func _ready():
-	pass
+	canon = canon_scene.instantiate()
+	#add_child(canon)
+	canon.position.x += 25
 
 func _physics_process(delta: float) -> void:
 	direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
@@ -30,9 +43,29 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_released("shoot"):
 		shoot_cannon()
+	
+	if Input.is_action_pressed("shoot"):
+		charge_canon(delta)
+		
+	rotate_canon()
 
-func charge_cannon():
-	pass
+func charge_canon(delta):
+	if charging == false:
+		shooting_dir = global_position.direction_to(get_global_mouse_position())
+		charge_current = 50
+	if current_ammo > 0 and charge_current < charge_max:
+		charging = true
+		charge_current += charge_speed * delta
+	elif current_ammo == 0:
+		# play error sound
+		return
+	charge_player_canon.emit(charge_current, shooting_dir, global_position)
 
 func shoot_cannon():
-	shoot_cannonball.emit(global_position, get_global_mouse_position(), self)	
+	shoot_cannonball.emit(global_position, get_global_mouse_position(), self, shooting_dir, charge_current)
+	charging = false
+	charge_current = -1
+	shooting_dir = 0
+
+func rotate_canon():
+	rotate_player_canon.emit(get_global_mouse_position(), global_position)
